2289eb52be0234d657d2b618d71b23cf842e0518
Markus Lindberg <markus.lindberg073@gmail.com>
2017-12-27 15:57:30 +0200

    Started work on custom number picker

---
+++ app/src/main/java/com/example/markus/customdictionary/CustomNumberPicker.java
@@ -0,0 +1,24 @@
+package com.example.markus.customdictionary;
+
+import android.content.Context;
+import android.widget.Button;
+import android.widget.TextView;
+
+/**
+ * Created by marku on 27.12.2017.
+ */
+
+public class CustomNumberPicker {
+    private int maxValue;
+    private int minValue;
+    private int currentValue;
+    private Button minus;
+    private Button plus;
+    private TextView number;
+    private Context cont;
+
+
+    public CustomNumberPicker(){}
+
+
+}
